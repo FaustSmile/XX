d53d445a5f4298842e080b22ca7ac69df6b2dbb4
FaustSmile <75768776+FaustSmile@users.noreply.github.com>
2026-05-31 17:35:28 +0800

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [require('tailwindcss-animate')],
-}
-export default config
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
